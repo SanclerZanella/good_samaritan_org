ba0661e0e5366e3a02d68d395c744c091c5d9d8a
Sancler Zanella <sanclerzjj@gmail.com>
2021-09-27 15:41:21 +0000

    Added navbar and menu to all screen sizes; Started build footer

---
+++ static/js/base.js
@@ -0,0 +1,10 @@
+$('#expand-menu-icon').click(() => {
+    $('.mobile-nav').show(300);
+    $('.mobile-nav').children().show(300);
+});
+
+$('.close-menu').click(() => {
+    $('.mobile-nav').hide(300);
+    $('.mobile-nav').children().hide(300);
+});
+
