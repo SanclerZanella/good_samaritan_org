c8aaa421d6b61c4c2e11757e8b39e3bde931ac6c
Sancler Zanella <sanclerzjj@gmail.com>
2021-10-04 11:08:27 +0000

    Finished product details template; Fix sort functionality to work on medium and small screens; Fix some UI elements in product, most-needed items and product view template to fit in different screen sizes

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -48,25 +48,40 @@ var infinite = new Waypoint.Infinite({
     }
 });
 
+$('.sort').each((key, value) => {
+    $(value).change(() => {
+        function slt(select) {
+            selector = $(select);
+            let currentUrl = new URL(window.location);
 
-// Reload the page on sort and return required criteria
-$('#sort').change(function() {
-    let selector = $(this);
-    let currentUrl = new URL(window.location);
+            let selectedVal = selector.val();
+            if(selectedVal != "reset"){
+                let sort = selectedVal.split("_")[0];
+                let direction = selectedVal.split("_")[1];
 
-    let selectedVal = selector.val();
-    if(selectedVal != "reset"){
-        let sort = selectedVal.split("_")[0];
-        let direction = selectedVal.split("_")[1];
+                currentUrl.searchParams.set("sort", sort);
+                currentUrl.searchParams.set("direction", direction);
 
-        currentUrl.searchParams.set("sort", sort);
-        currentUrl.searchParams.set("direction", direction);
+                window.location.replace(currentUrl);
+            } else {
+                currentUrl.searchParams.delete("sort");
+                currentUrl.searchParams.delete("direction");
 
-        window.location.replace(currentUrl);
-    } else {
-        currentUrl.searchParams.delete("sort");
-        currentUrl.searchParams.delete("direction");
+                window.location.replace(currentUrl);
+            }
+        };
 
-        window.location.replace(currentUrl);
-    }
+        slt_id = $(value).attr('id');
+
+        if (slt_id == 'sort-lg'){
+            slt(slt_element);
+        } else if(slt_id == 'sort-md') {
+            slt_element = $(value);
+            slt(slt_element);
+        } else if(slt_id == 'sort-sm') {
+            slt_element = $(value);
+            slt(slt_element);
+        }
+
+    });
 });
