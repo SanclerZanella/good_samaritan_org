3e969c158ab867c65af3938a074066d590966fd6
Sancler Zanella <sanclerzjj@gmail.com>
2021-10-09 13:15:18 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,9 @@
+import os
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import OrderForm
+if os.path.exists('env.py'):
+    import env
 
 
 def checkout(request):
@@ -11,9 +14,13 @@ def checkout(request):
     if 'cart' in request.session:
         cart = request.session.get('cart', {})
         order_form = OrderForm()
+        stripe_public_key = os.environ.get('STRIPE_PUBLIC_KEY', '')
+        client_secret = os.environ.get('CLIENT_SECRET', '')
 
         context = {
             'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': client_secret,
         }
 
         template = 'checkout/checkout.html'
--- env-sample.py
+++ env-sample.py
@@ -2,3 +2,5 @@ import os
 
 
 os.environ["DJANGO_SECRET"] = "Your Django Secret"
+os.environ["STRIPE_PUBLIC_KEY"] = "Your Stripe Public Key"
+os.environ["CLIENT_SECRET"] = "Your Stripe Client Secret"
