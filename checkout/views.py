f15776db99c9be7d7bc77bc0e4d0b6012c90a6bf
Sancler Zanella <sanclerzjj@gmail.com>
2021-10-09 16:28:01 +0000

    Added basic stripe checkout functionality in the checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,12 @@
 import os
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
-if os.path.exists('env.py'):
-    import env
+from cart.contexts import cart_contents
+import stripe
+# if os.path.exists('env.py'):
+#     import env
 
 
 def checkout(request):
@@ -12,15 +15,28 @@ def checkout(request):
     """
 
     if 'cart' in request.session:
-        cart = request.session.get('cart', {})
         order_form = OrderForm()
-        stripe_public_key = os.environ.get('STRIPE_PUBLIC_KEY', '')
-        client_secret = os.environ.get('CLIENT_SECRET', '')
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        client_secret = settings.STRIPE_SECRET_KEY
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = client_secret
+
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
 
         context = {
             'order_form': order_form,
             'stripe_public_key': stripe_public_key,
-            'client_secret': client_secret,
+            'client_secret': intent.client_secret,
         }
 
         template = 'checkout/checkout.html'
