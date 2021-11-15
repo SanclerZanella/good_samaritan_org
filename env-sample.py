cfc21bd2ba4065252f7c22289eebc7fadf21d533
Sancler Zanella <sanclerzjj@gmail.com>
2021-11-15 18:04:04 +0000

    Setup to deploy static file to s3 bucket

--- env-sample.py
+++ env-sample.py
@@ -7,6 +7,8 @@ os.environ["CLIENT_SECRET"] = "Your Stripe Client Secret"
 os.environ["STRIPE_WH_SECRET"] = "Your Stripe Webhook Secret"
 os.environ["EMAIL_HOST_PASS"] = "Your email host password"
 os.environ["EMAIL_HOST_USER"] = "Your email"
+os.environ["AWS_ACCESS_KEY_ID"] = "From downloaded amazon CSV file"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "From downloaded amazon CSV file"
 
 # This variable should not be added in production
 os.environ["DEVELOPMENT"] = "true"
