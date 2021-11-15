b8f1e088b32427e3dedefb9ef72f97bdd9937845
Sancler Zanella <sanclerzjj@gmail.com>
2021-11-15 19:07:52 +0000

    Try to fix resource load from s3 bucket

--- good_samaritan/settings.py
+++ good_samaritan/settings.py
@@ -203,7 +203,7 @@ if 'USE_AWS' in os.environ:
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
     # Override static and media URLs in production
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    STATIC_URL = 's3://good-samaritan-org/static/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
