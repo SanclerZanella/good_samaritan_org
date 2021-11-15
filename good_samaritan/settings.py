b3f3fca7b86397fe32db4fd95633a78b75853797
Sancler Zanella <sanclerzjj@gmail.com>
2021-11-15 19:47:14 +0000

    Try to fix internal error in heroku deployment

--- good_samaritan/settings.py
+++ good_samaritan/settings.py
@@ -133,16 +133,16 @@ WSGI_APPLICATION = 'good_samaritan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 if 'DATABASE_URL' in os.environ:
+    DATABASE_URL = os.environ.get('DATABASE_URL', '')
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
-else:
-    DATABASE_URL = os.environ.get('DATABASE_URL', '')
-    DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL)
     }
 
 # Password validation
