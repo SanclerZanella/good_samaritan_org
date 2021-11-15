932f3d3e455b2068a84a73d496ef42cb91f9e4aa
Sancler Zanella <sanclerzjj@gmail.com>
2021-11-15 13:55:16 +0000

    Created heroku app for this project; Added remote database to DTABASES configuration in settings.py

--- good_samaritan/settings.py
+++ good_samaritan/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.exists('env.py'):
     import env
 
@@ -129,13 +130,18 @@ WSGI_APPLICATION = 'good_samaritan.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DEVELOPMENT' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASE_URL = os.environ.get('DATABASE_URL', '')
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
