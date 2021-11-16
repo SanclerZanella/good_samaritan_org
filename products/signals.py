4ee218ab4044cc52cbd70a16ba5394cbac3d7118
Sancler Zanella <sanclerzjj@gmail.com>
2021-11-16 00:00:40 +0000

    Fix products image path

--- products/signals.py
+++ products/signals.py
@@ -35,7 +35,7 @@ def auto_delete_file_on_change(sender, instance, **kwargs):
     new_file = instance.image
 
     if not old_file == new_file:
-        if old_file.path:
+        if old_file:
             if os.path.isfile(old_file.path):
                 os.remove(old_file.path)
 
